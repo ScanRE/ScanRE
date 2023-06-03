97b7fcbb561bcd6010083dae0ee7e597744c60ef
HardikRa <raheja.hardik@gmail.com>
2023-06-03 17:15:10 +0530

    chore: setup Navbar

---
+++ mh-frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return ( 
+        <nav>
+            <img src="" alt="" className="logo" />
+            <ul>
+                <li><a href="/">Features</a></li>
+                <li><a href="/">Plan</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+        </nav> 
+    );
+}
+
+export default Navbar;
---
+++ mh-frontend/src/containers/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <Navbar/>
+    </div> 
+  )
+}
+
+export default Landing
