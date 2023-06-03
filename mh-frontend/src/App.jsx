cf7bb23538562931890872e078e5dca813006efc
saket <saketthota98@gmail.com>
2023-06-03 16:59:32 +0530

    Routes setup

--- mh-frontend/src/App.jsx
+++ mh-frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
-import Landing from './containers/Landing'
 import { Routes, Route } from "react-router-dom";
+import Landing from './containers/Landing'
+import Home from './containers/Home';
+import FindingDetail from './containers/FindingDetail';
 
 function App() {
   return (
       <>
           <Routes>
-              <Route path="/" element={<Landing />}></Route>
+              <Route path="/" element={<Landing />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/findingDetail/*" element={<FindingDetail/>}/>
           </Routes>
       </>
   );
--- mh-frontend/src/containers/Home.jsx
+++ mh-frontend/src/containers/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
+      <div>
+          <div>
+              <div className="tabs tabs-boxed">
+                  <a className="tab">Tab 1</a>
+                  <a className="tab tab-active">Tab 2</a>
+                  <a className="tab">Tab 3</a>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default Home
